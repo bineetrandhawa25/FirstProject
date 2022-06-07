d51142c62bcedc995a696e5b0ed0c712cfd85f2e
bineetrandhawa25 <63625517+bineetrandhawa25@users.noreply.github.com>
2022-06-06 21:09:30 -0400

    Update TestingGit.java

--- src/TestingGit.java
+++ src/TestingGit.java
@@ -2,9 +2,12 @@
 public class TestingGit {
 
 	public static void main(String[] args) {
+		//trying to edit this file in GIT and commiting it here
+		int a=2;
+		int b=3;
+		System.out.println("A + B : " + a+b);
 		
 		
-		System.out.println(" making changes here first and THEN checking if it reflects in git log command");
 
 	}
 
