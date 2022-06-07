808b2a48517aacf4ab85453818c286c4f34874cc
bineetrandhawa25 <bineetrandhawa25@gmail.com>
2022-06-06 21:55:37 -0400

    verifying merge

--- src/TestingGit.java
+++ src/TestingGit.java
@@ -2,7 +2,16 @@
 public class TestingGit {
 
 	public static void main(String[] args) {
+<<<<<<< HEAD
 		// TODO Auto-generated method stub
+=======
+		//trying to edit this file in GIT and commiting it here
+		int a=2;
+		int b=3;
+		System.out.println("A + B : " + a+b);
+		
+		
+>>>>>>> d51142c62bcedc995a696e5b0ed0c712cfd85f2e
 
 	}
 
